c7d4ea681f81dcaed6ea0d4647b072d8eb3de545
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-08-21 21:00:22 -0400

    Update salt.js

--- app/auth/salt.js
+++ app/auth/salt.js
@@ -1,9 +1,9 @@
+/*
+As much as I wish there would be pepper.js as well, thats not the case. A salt is a random piece of data that is used as an additional input to a one-way function that hashes data or a password.
+*/
+
 import btoa from 'btoa'
 
-/**
- * Generate 16 bytes salt for bcrypt by seed. Should return the same salt for the same seed.
- * @param  {string} seed The seed for salt
- */
 export default function (seed) {
   const bytes = []
 
@@ -11,14 +11,11 @@ export default function (seed) {
     bytes.push(seed.charCodeAt(i))
   }
 
-  // Salt must be 16 bytes
   while (bytes.length < 16) {
     bytes.push(0)
   }
 
-  // Convert byte array to base64 string
   const salt = btoa(String.fromCharCode.apply(String, bytes.slice(0, 16)))
 
-  // Adding header for bcrypt. Fake 10 rounds.
   return '$2a$10$' + salt
 };
